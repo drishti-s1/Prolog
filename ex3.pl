2ed376ff8e2c9958dc7f3a8904aa19077f42038f
Drishti <111050457+drishti-s1@users.noreply.github.com>
2023-04-24 08:40:15 +0530

    Add files via upload

---
+++ ex3.pl
@@ -0,0 +1,9 @@
+buys(nita,vegetables(tomato)).
+buys(priya,fruits(mango)).
+buys(sreya,vegetables(potato)).
+buys(rahul,vegetables(cauliflower)).
+
+juicy(vegetables(tomato)).
+seasonal(fruits(mango)).
+boil(vegetables(potato,cauliflower)).
+
